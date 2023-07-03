8f25dad079ea4e9aa31a1009b8656dcdaae0728f
rostami2001 <123289362+rostami2001@users.noreply.github.com>
2023-07-03 10:23:24 +0330

    Update phase_3.py

--- phase-3/phase-3/phase_3.py
+++ phase-3/phase-3/phase_3.py
@@ -43,6 +43,59 @@ for row in result:
 
 #/zahra_rostami
 
+#view1
+
+view_query = "create view view1_p as select course.course_code,course.course_name,class.class_number,course.professor_coname,course.faculty from course,class where course.course_code = class.class_id"
+mycursor.execute(view_query)
+
+select_query = "select * from view1_p"
+mycursor.execute(select_query)
+
+result = mycursor.fetchall()
+
+for row in result:
+  print(row)
+
+#view5
+
+view_query = "CREATE VIEW view5_p AS SELECT student.student_number,student.student_name FROM student,select_unit WHERE select_unit.passed_course < 12 AND student.student_number = select_unit.student_student_number"
+mycursor.execute(view_query)
+
+select_query = "SELECT * FROM view5_p"
+mycursor.execute(select_query)
+
+result = mycursor.fetchall()
+
+for row in result:
+ print(row)
+
+#trigger2
+
+trigger_query = """
+CREATE TRIGGER trigger2_p AFTER INSERT ON food_reservation FOR EACH ROW 
+BEGIN
+update student
+set financial_balance = financial_balance - ( select price from food,food_reservation
+     where (food.food_reservation_reserve_fid = food_reservation.reserve_fid) and new.food_reservation.reserved = '1' )
+     where student_number = new.food_reservation.student_student_number;
+END
+"""
+mycursor.execute(trigger_query)
+
+
+#trigger ex1
+
+trigger_query = """
+CREATE TRIGGER ex1 AFTER UPDATE ON food_reservation FOR EACH ROW 
+BEGIN
+if new.eaten = 1 then
+update food_reservation
+set forgot = 0;
+End if ;
+END
+"""
+mycursor.execute(trigger_query)
+
 #/elaheh_rezapanah
 
 # view4
